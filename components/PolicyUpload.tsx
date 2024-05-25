ada94bd330f6b64fd317cc6dbdc1ee2de340f916
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-05-25 17:32:47 +0800

    upload

---
+++ components/PolicyUpload.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export const PolicyUpload = () => (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input id="policy" type="file" placeholder="Policy Upload"/>
+        <Button>Submit</Button>
+    </div>
+)
