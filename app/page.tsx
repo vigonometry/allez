a6d5bcae6042d1a56a657e5b7d4b4e20d35892c8
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-05-26 07:55:49 +0800

    pls test b4 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
+
 "use client";
 import { PolicyUpload } from "@/components/PolicyUpload";
-import { BreakdownChart } from "@/components/BreakdownChart";
+import { BreakdownChart} from "@/components/BreakdownChart";
 import { examplePolicies } from "@/sample_data/samplePolicies";
 import MultiSelect from "@/components/DropDown";
-import { CardVariable } from "../components/CardVariable";
-import { PolicyCards } from "../components/PolicyCards";
-import React, { useState } from "react";
+import { PolicyCards } from "../components/PolicyCards"
+import React, { useState } from 'react';
+
+
 
 export default function Home() {
-  const [lang, setLang] = useState<string>("English");
-
-  const [label, setLabel] = useState<any>();
   const [selectedPolicies, setSelectedPolicies] = useState<string[]>([]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-4xl">
-          Hi, Welcome to your Safety Net &#128075;
-        </h1>
+      Hi, Welcome to your Safety Net &#128075;
+      </h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <PolicyUpload />
+        <PolicyUpload />
         </div>
+        
       </div>
 
     <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:grid-cols-4 lg:text-left">
@@ -28,16 +28,14 @@ export default function Home() {
       <div className="p-4">
       <MultiSelect setSelectedPolicies={setSelectedPolicies} />
       </div>
-      <BreakdownChart policies={selectedPolicies.length === 0 ? examplePolicies : examplePolicies.filter(x => selectedPolicies.includes(x.policy_name.toLowerCase().replace(/\s/g, '_')))} policyType='accidental_death'/>
+        <BreakdownChart policies={selectedPolicies.length === 0 ? examplePolicies : examplePolicies.filter(x => selectedPolicies.includes(x.policy_name.toLowerCase().replace(/\s/g, '_')))} policyType='accidental_death'/>
       </div>
 
         <div className="col-start-3 row-start-1 p-4 ">
-          <PolicyCards
-            selectedPolicies={selectedPolicies}
-            policies={examplePolicies}
-          />
+      <PolicyCards selectedPolicies={selectedPolicies} policies={examplePolicies} />
         </div>
-      </div>
+    </div>
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -1,26 +1,20 @@
-import React, { Dispatch, SetStateAction } from "react";
-import MultipleSelector, { Option } from "./ui/multiple-selector";
-import { examplePolicies } from "../sample_data/samplePolicies";
+import React, { useState } from 'react';
+import MultipleSelector, { Option } from './ui/multiple-selector';
+import { examplePolicies } from '../sample_data/samplePolicies'; 
 
 const OPTIONS: Option[] = examplePolicies.map((policy) => ({
   label: policy.policy_name,
-  value: policy.policy_name.toLowerCase().replace(/\s/g, "_"),
+  value: policy.policy_name.toLowerCase().replace(/\s/g, '_'), 
 }));
 
 interface MultiSelectProps {
   setSelectedPolicies: (selected: string[]) => void;
 }
 
-const MultiSelect: React.FC<
-  MultiSelectProps & {
-    setData: Dispatch<SetStateAction<any>>;
-  }
-> = (item, { setSelectedPolicies, setData }) => {
+const MultiSelect: React.FC<MultiSelectProps> = ({ setSelectedPolicies }) => {
   const handleSelectionChange = (selectedOptions: Option[]) => {
-    const selectedValues = selectedOptions.map((option) => option.value);
+    const selectedValues = selectedOptions.map(option => option.value);
     setSelectedPolicies(selectedValues);
-
-    setData(item);
   };
 
   return (
