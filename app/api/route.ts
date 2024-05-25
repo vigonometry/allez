247ea512c2e5af37e8077b37a1c26634a1a1db3e
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-05-26 07:40:02 +0800

    Revert "add route"
    This reverts commit 603e7f6a653447009e89f89c6efc16697728d048.

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,12 @@
-// const data = {
-//     total_permanent_disability: 0,
-//     death: 0,
-//     critical_illness: 0,
-//     health_insurance: 0,
-//     accidental_death: 300000,
-//     accidental_tpd: 100000,
-// }
+const data = {
+    total_permanent_disability: 0,
+    death: 0,
+    critical_illness: 0,
+    health_insurance: 0,
+    accidental_death: 300000,
+    accidental_tpd: 100000,
+}
 
-export async function getPolicyData() {
-    const response = await fetch("http://localhost:5000/policies");
-    return response.json();
+export async function GET(request: Request) {
+    return Response.json(data)
 }
