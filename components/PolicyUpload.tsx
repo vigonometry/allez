650d08af5324a54916798d95a94e5d90f2ca0ec5
mochamadsatria <mochamad.sat2002@mail.ugm.ac.id>
2024-05-25 21:23:42 +0800

    add backend

--- components/PolicyUpload.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-
-export const PolicyUpload = () => (
-    <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input id="policy" type="file" placeholder="Policy Upload"/>
-        <Button>Submit</Button>
-    </div>
-)
