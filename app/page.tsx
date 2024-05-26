a20d6806925d6f519ffdccd72689a0d560d15352
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-05-26 08:37:52 +0800

    remove next

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import { examplePolicies } from "@/sample_data/samplePolicies";
 import MultiSelect from "@/components/DropDown";
 import { PolicyCards } from "../components/PolicyCards"
 import React, { useState } from 'react';
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
 
 
 
 export default function Home() {
   const [selectedPolicies, setSelectedPolicies] = useState<string[]>([]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
@@ -23,89 +25,22 @@ export default function Home() {
         
       </div>
 
-    <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:grid-cols-4 lg:text-left">
+    <div className="mb-32 grid grid-cols-3 text-center lg:mb-0 lg:w-full lg:text-left">
       <div className="col-start-1 row-start-1 p-4">
-      <div className="p-4">
-      <MultiSelect setSelectedPolicies={setSelectedPolicies} />
+      <h2 className="place-items-center text-xl font-semibold">Breakdown</h2>
+      <div className="py-4">
+        <div className="w-full flex-row">
+          <MultiSelect setSelectedPolicies={setSelectedPolicies} />
+          <DropdownMenu />
+        </div>
       </div>
         <BreakdownChart policies={selectedPolicies.length === 0 ? examplePolicies : examplePolicies.filter(x => selectedPolicies.includes(x.policy_name.toLowerCase().replace(/\s/g, '_')))} policyType='accidental_death'/>
       </div>
 
-        <div className="col-start-3 row-start-1 p-4 ">
-      <PolicyCards selectedPolicies={selectedPolicies} policies={examplePolicies} />
+        <div className="col-start-2 row-start-1 ">
+        <PolicyCards selectedPolicies={selectedPolicies} policies={examplePolicies} />
         </div>
     </div>
-
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
