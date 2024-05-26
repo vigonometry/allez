c4e7f6fe1bedf83ff275e15a823b853fa5fc6779
liankokhai <unfazing@gmail.com>
2024-05-26 10:06:28 +0800

    defaults

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { CoverageDropdown } from "@/components/ComboBox";
 
 export default function Home() {
   const [selectedPolicies, setSelectedPolicies] = useState<string[]>([]);
-  const [selectedCoverage, setSelectedCoverage] = useState<string>("critical_illness");
+  const [selectedCoverage, setSelectedCoverage] = useState<string>("accidental_death");
 
   const [policiesD, setPoliciesD] = useState<any>([]);
 
